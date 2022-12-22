b249129d7aa374c557c0ebc0475301a51f760332
beckqing <beck@beckqing.com>
2022-12-22 12:03:42 -0500

    Cleaning up and del unneeded files

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -2,7 +2,7 @@ if (window.netlifyIdentity) {
   window.netlifyIdentity.on("init", user => {
     if (!user) {
       window.netlifyIdentity.on("login", () => {
-        document.location.href = "/cms/";
+        document.location.href = "/_cms/";
       });
     }
   });
