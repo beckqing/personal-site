a4bb5f66a452be2ed879f066147113af67ead395
beckqing <beck@beckqing.com>
2022-12-22 11:22:56 -0500

    Cleaning up and del unneeded files

--- .eleventy.js
+++ .eleventy.js
@@ -78,7 +78,7 @@ module.exports = function(eleventyConfig) {
   // Don't process folders with static assets e.g. images
   eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.addPassthroughCopy("static/img");
-  eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("cms");
   eleventyConfig.addPassthroughCopy("_includes/assets/");
 
   /* Markdown Plugins */
